5702e2836a6e474ec488c2c144f00ffeea59c27b
KaioRapanos <kaiorapanos@gmail.com>
2024-01-08 19:38:32 -0300

    Vararg Concepts

--- main.kt
+++ main.kt
@@ -18,6 +18,7 @@ fun sum(x: Int, y: Int): Int {
 fun multiply(x: Int, y: Int) = x * y
 
 fun main(){
+
     hello()
     printMessage("Hello!")
     printMessageWithPrefix("Hello", "log")
@@ -25,5 +26,28 @@ fun main(){
     printMessageWithPrefix(prefix =  "log", message = "Hello!")
     println(sum(1,2))
     println(multiply(2,4))
+
+    fun printAll(vararg message: String){
+        for(iten in message) println(iten)
+    }
+
+    printAll("Hello !", "Sou Kaio", "Muito Prazer")
+
+    fun printAllWithPrefix(vararg message: String, prefix: String){
+        for(iten in message) println(prefix + iten)
+    }
+
+    printAllWithPrefix(
+        "Hello", "Mundo", "Thanks",
+        prefix = "Greeting: "
+    )
+
+    fun log(vararg entries: String){
+        printAll(*entries)
+    }
+
+    log("Hello !", "Sou Kaio", "Muito Prazer")
+
+
 
 }
