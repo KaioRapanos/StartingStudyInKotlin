f9cb716632cfb70038b9e96eeba85e2a8743ddcf
KaioRapanos <kaiorapanos@gmail.com>
2024-01-09 20:49:00 -0300

    Working with generics and stacks

--- main.kt
+++ main.kt
@@ -24,6 +24,23 @@ class Customer
 
 class Contact(val id: Int, var email: String)
 
+class MutableStack<E>(vararg items: E) {
+
+    private val elements = items.toMutableList()
+
+    fun push(element: E) = elements.add(element)
+
+    fun peek(): E = elements.last()
+
+    fun pop(): E = elements.removeAt(elements.size - 1)
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun size() = elements.size
+
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
 fun main(){
 
     hello()
@@ -106,5 +123,20 @@ fun main(){
     println("${contact.id}  ${contact.email}")
     contact.email = "jane@gmail.com"
     println("${contact.id}  ${contact.email}")
+
+    //Generics
+
+    val stack = MutableStack(0.62, 3.14, 2.7)
+    stack.push(9.87)
+    println(stack)
+
+    println("peek(): ${stack.peek()}")
+    println(stack)
+    
+    for (i in (1)..stack.size()){
+        println("pop: ${stack.pop()}")
+        println(stack)
+    }
+
 
 }
