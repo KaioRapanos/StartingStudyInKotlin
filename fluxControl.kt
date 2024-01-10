5dce67a79a265fea14364eac41aeb561760f3e82
KaioRapanos <kaiorapanos@gmail.com>
2024-01-10 20:16:01 -0300

    Working with Ranges

--- fluxControl.kt
+++ fluxControl.kt
@@ -40,6 +40,44 @@ fun main() {
         println("Watch out, it`s a ${animals.name}")
     }
 
+    //Ranges
+
+    for(i in 0..3){
+        println(i)
+    }
+    println(" ")//0123
+
+    for (i in 0 until 3){
+        println(i)
+    }
+    println(" ")//012
+
+    for (i in 2 .. 8 step 2){
+        println(i)
+    }
+    println(" ")//2468
+
+    for (i in 3 downTo 0){
+        println(i)
+    }
+    println(" ")//3210
+
+    //Char ranges are also supported
+    for (c in 'a'..'d'){
+        println(c)
+    }
+    println(" ")
+
+    val x = 2
+    if (x in 1 .. 5){
+        println("x is in range from 1 to 5")
+    }
+    println(" ")
+
+    if (x !in 6 .. 10){
+        println("x is not in range from 6 to 10")
+    }
+
 }
 
 class Animal(val name: String)
