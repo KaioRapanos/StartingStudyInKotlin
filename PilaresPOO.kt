d81096ee1338494f7bc78cb6a3d97af41f3fa1f4
KaioRapanos <kaiorapanos@gmail.com>
2024-01-12 20:58:48 -0300

    Working with Data Class

--- PilaresPOO.kt
+++ PilaresPOO.kt
@@ -36,8 +36,39 @@ fun main(){
 
     val lion: Lion = Asiatic("Rufo")
     lion.sayHello()
+
+    val user = User("Alex",1)
+    println(user)
+
+    val secondUser = User("Alex",1)
+    val thirdUser = User("Max",2)
+
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+    //hashCode()
+    println(user.hashCode())
+    println(secondUser.hashCode())
+    println(thirdUser.hashCode())
+
+    //copy()
+    println(user.copy())
+    println(user === user.copy())
+    println(user.copy("Max"))
+    println(user.copy(id = 3))
+
+    println("name = ${user.component1()}")
+
+
 }
 
 //Data Classes
+
+data class User(val name: String, val id: Int){
+    override fun equals(other: Any?) =
+        other is User && other.id == this.id
+}
+
+
 //Enum Classes
 //Sealed Classes
